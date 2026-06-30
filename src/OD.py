501f875a804ab54d8228c97f9c631219c392d530
takuya <e245745@ie.u-ryukyu.ac.jp>
2026-06-30 10:42:52 +0900

    相対パスに変更

--- src/OD.py
+++ src/OD.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+from pathlib import Path
 
 def main():
     print("🚀 ファインチューニング（再学習）を開始します...")
@@ -6,9 +7,12 @@ def main():
     # 1. ベースとなる「賢い脳みそ」を読み込む
     model = YOLO('yolov8n.pt')
 
+    base_dir = Path(__file__).resolve().parent
+    data_path = base_dir.parent / "my_bus_dataset_robust"
+
     # 2. オリジナルデータセットを使って学習をスタート！
     results = model.train(
-        data='/Users/yooseiisikawa/sakuhin/IP/my_bus_dataset_robust',  # 先ほど自動生成した指示書のパス
+        data=str(data_path / "dataset.yaml"),  # 先ほど自動生成した指示書のパス
         epochs=50,                           # 学習を繰り返す回数（今回は少なめなので50回）
         imgsz=640,                           # 画像のサイズ（標準の640）
         device='mps',                        # M2 Macの専用コア（MPS）をフル活用！
